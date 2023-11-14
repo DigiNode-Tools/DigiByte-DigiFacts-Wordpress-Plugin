bb6ba40c52a34c07750d8da96f89cd60d0d14765
saltedlolly <olly@stedall.com>
2023-11-14 18:03:46 +0000

    Add GPL plugin licence

--- digibyte-digifacts/digibyte-digifacts.php
+++ digibyte-digifacts/digibyte-digifacts.php
@@ -1,9 +1,27 @@
 <?php
+/**
+* Plugin Name:   DigiByte DigiFacts
+* Plugin URI:    https://github.com/saltedlolly/DigiByte-DigiFacts-Wordpress-Plugin
+* Description:   Display random DigiFacts about DigiByte in multiple languages.
+* Version:       1.1
+* Author:        Olly Stedall (DigiByte.Help)
+* Text Domain:   digibyte-digifacts
+*/
+
 /*
-Plugin Name: DigiByte DigiFacts
-Description: Display random DigiFacts about DigiByte in multiple languages.
-Version: 1.1
-Author: Olly Stedall (DigiByte.Help)
+DigiByte DigiFacts is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+DigiByte DigiFacts is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with DigiByte DigiFacts. If not, see:
+https://github.com/saltedlolly/DigiByte-DigiFacts-Wordpress-Plugin
 */
 
 add_action('admin_menu', 'digibyte_digifacts_admin_menu');
